5348537e600e74b464f11694b23b92b9293e7dd5
Apal Shah <Apal Shah>
2017-02-19 11:46:59 +0530

    index & contact

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.querySelector(".sidebar-btn").addEventListener("click", function() {
+
+
+	if ( document.querySelector(".sidebar").classList.contains('sidebar-show') ) {
+		document.querySelector(".sidebar").classList.remove('sidebar-show');
+		document.querySelector(".sidebar-btn").classList.remove('sidebar-btn-close');		
+	} else {
+		document.querySelector(".sidebar").classList.add('sidebar-show');
+		document.querySelector(".sidebar-btn").classList.add('sidebar-btn-close');
+	}
+
+});
+
+
+document.querySelector('.sidebar-btn').addEventListener('click', function(event) {
+	event.preventDefault();
+});
+
+function delay (URL) {
+    setTimeout( function() { window.location = URL }, 300 );
+    $('body').addClass('body');
+}
