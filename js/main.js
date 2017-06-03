38b973ff6fc50203281e49307283143acca473c9
Apal Shah <Apal Shah>
2017-06-03 15:26:15 +0530

    HTML5 canvas signature

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector(".sidebar-btn").addEventListener("click", function() {
 
 	if ( document.querySelector(".sidebar").classList.contains('sidebar-show') ) {
 		document.querySelector(".sidebar").classList.remove('sidebar-show');
-		document.querySelector(".sidebar-btn").classList.remove('sidebar-btn-close');		
+		document.querySelector(".sidebar-btn").classList.remove('sidebar-btn-close');
 	} else {
 		document.querySelector(".sidebar").classList.add('sidebar-show');
 		document.querySelector(".sidebar-btn").classList.add('sidebar-btn-close');
